fbb20082027414d2789987b28f3e4ad01b0037ab
rorysaur <ou.rory@gmail.com>
2014-01-20 14:36:15 -0800

    PhotoDetail view like/unlike buttons

--- app/assets/javascripts/models/photo.js
+++ app/assets/javascripts/models/photo.js
@@ -1,3 +1,10 @@
 Silverprint.Models.Photo = Backbone.Model.extend({
   
+  isLikedBy: function (user) {
+    var notLiked = this.get("likers").every(function (liker) {
+      return liker.id != user.id;
+    });
+    
+    return !notLiked;
+  }
 });
